b6b6fdcdace83bd5cd500c48942b05a585483ac8
xandervr <xandervanraemdonck@gmail.com>
2020-03-06 10:12:05 +0100

    fix consumer queuename

--- src/consumer.js
+++ src/consumer.js
@@ -2,28 +2,27 @@
 
 const Instance = require('./instance');
 
-
 class Consumer extends Instance {
-    /**
-     * See docs.
-     *
-     * @param {object} config
-     * @param {object} options
-     */
-    constructor(config = {}, options = {}) {
-        super();
-        this.dispatcher.bootstrapConsumer(this, config, options);
-    }
+  /**
+   * See docs.
+   *
+   * @param {object} config
+   * @param {object} options
+   */
+  constructor(config = {}, options = {}, queueName) {
+    super();
+    this.dispatcher.bootstrapConsumer(this, config, options, queueName);
+  }
 
-    /**
-     *
-     * @param {*} message
-     * @param {function} cb
-     */
-    consume(message, cb) {
-        /* eslint class-methods-use-this: 0 */
-        throw new Error('Consume method should be extended');
-    }
+  /**
+   *
+   * @param {*} message
+   * @param {function} cb
+   */
+  consume(message, cb) {
+    /* eslint class-methods-use-this: 0 */
+    throw new Error('Consume method should be extended');
+  }
 }
 
 module.exports = Consumer;
